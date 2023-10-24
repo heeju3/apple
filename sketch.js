787ee9efd8d95247e434277a6cad2a61d07ad11c
heeju3 <144978953+heeju3@users.noreply.github.com>
2023-10-24 17:04:05 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -19,12 +19,10 @@ function setup() {
   
   switchBtn = createButton('Switch');
   switchBtn.position(displayWidth/2,10);
-  switchBtn.size(250,50);
+  switchBtn.size(150,50);
   switchBtn.mouseReleased(switchCam);
   
-  
-  
-  
+
   rearSetting = {
     audio: false,
     video: {
@@ -44,7 +42,7 @@ function setup() {
   }
   
   cam = createCapture(frontSetting);
-  cam.size(displayWidth,661);// 이 명령어 추가해주었습니다.
+  cam.size(displayWidth,661);
   cam.hide();
   
   detector.detect(cam, gotDetections);
@@ -56,18 +54,18 @@ function switchCam(){
     switcher = 1;
     cam.remove();
     cam = createCapture(rearSetting);
-    cam.size(displayWidth,661); // 이 명령어 추가해주었습니다.
+    cam.size(displayWidth,661);
     cam.hide();
     
-    detector.detect(cam, gotDetections);//cam 을 지우고 새롭게 createCapture 를 하였기 때문에, front/rear 카메라 변경될 때마다 이 명령어를 한 번 더 적어주어야 합니다.
+    detector.detect(cam, gotDetections);
   }else if(switcher == 1){
     switcher = 0;
     cam.remove();
     cam = createCapture(frontSetting);
-    cam.size(displayWidth,661);// 이 명령어 추가해주었습니다.
+    cam.size(displayWidth,661);
     cam.hide();
     
-    detector.detect(cam, gotDetections); //cam 을 지우고 새롭게 createCapture 를 하였기 때문에, front/rear 카메라 변경될 때마다 이 명령어를 한 번 더 적어주어야 합니다.
+    detector.detect(cam, gotDetections);
   }
 }
 
@@ -83,31 +81,26 @@ function draw() {
   ellipse(width/2,800,50);
 }
   
-  for (let i = 0; i < detectedObjects.length; i++) {
-    let object = detectedObjects[i];
-      if(object.label == 'apple'){
-        stroke(0,255,0);
-        strokeWeight(4);
-        noFill();
-        rect(object.x, object.y, object.width, object.height);
-        noStroke();
-        fill(0,255,0);
-        textSize(24);
-        text(object.label, object.x + 10, object.y+24);
-  }
-}
 
-function mouseReleased(){
-  if(dist(mouseX, mouseY, width/2, 400) <= 35){
-    let fileName = 'myCanvas_'+day()+hour()+minute()+second()+'.jpg'
-    saveCanvas(image, fileName);
-  }
-}
 
 function gotDetections(error, results) {
   if (error) {
     console.error(error);
   }
+  
+  for (let i = 0; i < detectedObjects.length; i++) {
+    let object = detectedObjects[i];
+    if(object.label == 'apple'){
+      stroke(0,255,0);
+      strokeWeight(4);
+      noFill();
+      rect(object.x, object.y, object.width, object.height);
+      noStroke();
+      fill(0,255,0);
+      textSize(24);
+      text(object.label, object.x + 10, object.y+24);
+  }
+}
   
   detectedObjects = results;
   detector.detect(cam, gotDetections);
